cec06d81063c4d48f8d8e84eb1099440abe6c43e
Vik63 <sfoto116@yandex.ru>
2022-07-27 22:03:58 +0400

    16-8

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,10 +1,14 @@
 const ErrorEmail = require('../errors/errorEmail');
+const ErrorAuth = require('../errors/errorAuth');
 
 const handleErrors = (err, req, res, next) => {
   const DUPLICATE_ERROR_CODE = 11000;
 
   if (err.code === DUPLICATE_ERROR_CODE) {
     throw new ErrorEmail('При регистрации указан email, который уже существует на сервере');
+  }
+  if (err.message === 'Неправильные почта или пароль') {
+    throw new ErrorAuth('Неправильные почта или пароль');
   }
   next(err);
 };
